8479ea018a13ac607444881a401f8c014a3ad885
kentaro84207 <kentaro84207@gmail.com>
2020-04-12 17:39:07 +0900

    Implement delete function

--- src/components/common/list/problemList.tsx
+++ src/components/common/list/problemList.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { Problem } from 'services/models/problem';
+import deleteProblem from 'services/delete-problem';
+import { FirebaseContext } from 'contexts';
+import paths from 'paths';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
@@ -11,7 +14,16 @@ import EditIcon from '@material-ui/icons/Edit';
 import DeleteIcon from '@material-ui/icons/Delete';
 
 const ProblemList: React.FC<{ problem: Problem }> = ({ problem }) => {
+  const { db } = useContext(FirebaseContext);
   const { name, id } = problem;
+  const history = useHistory();
+
+  const handleDeleteProblem = (e: React.MouseEvent<HTMLElement>) => {
+    e.preventDefault();
+    const confirmText = `${name}を削除しますか？`;
+    if (window.confirm(confirmText) && db && id) deleteProblem(db, id);
+    history.replace(paths.home);
+  };
 
   return (
     <>
@@ -21,7 +33,11 @@ const ProblemList: React.FC<{ problem: Problem }> = ({ problem }) => {
         </ListItemIcon>
         <ListItemText primary={name} />
         <ListItemSecondaryAction>
-          <IconButton edge="end" aria-label="delete">
+          <IconButton
+            edge="end"
+            aria-label="delete"
+            onClick={handleDeleteProblem}
+          >
             <DeleteIcon />
           </IconButton>
         </ListItemSecondaryAction>
