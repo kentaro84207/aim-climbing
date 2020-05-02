37dc9cc122063b01588daf28ec5476e96dfdd981
kentaro84207 <kentaro84207@gmail.com>
2020-05-02 11:13:31 +0900

    Implement auth check

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import paths from 'paths';
 import Home from './components/Home';
 import Result from './components/Result';
@@ -10,29 +10,36 @@ import ProblemCreate from './components/Problem/create';
 import ProblemEdit from './components/Problem/edit';
 import Signin from './components/Signin';
 import Signup from './components/Signup';
+import AuthRedirect from './containers/Auth/AuthRedirect';
 
 const App = () => {
   return (
     <div className="App">
-      <Switch>
-        <Route path={paths.home} component={Home} exact />
-        <Route path={paths.problem} component={Problem} exact />
-        <Route path={paths.create} component={ProblemCreate} exact />
-        <Route
-          path={`${paths.edit}/:problemId`}
-          component={ProblemEdit}
-          exact
-        />
-        <Route
-          path={`${paths.problem}/:problemId`}
-          component={ProblemShow}
-          exact
-        />
-        <Route path={paths.result} component={Result} />
-        <Route path={paths.mypage} component={Mypage} />
-        <Route path={paths.signin} component={Signin} />
-        <Route path={paths.signup} component={Signup} />
-      </Switch>
+      <Router>
+        <Switch>
+          <Route path={paths.signin} component={Signin} />
+          <Route path={paths.signup} component={Signup} />
+          <Route path={paths.home} component={Home} exact />
+          <AuthRedirect>
+            <Switch>
+              <Route path={paths.problem} component={Problem} exact />
+              <Route path={paths.create} component={ProblemCreate} exact />
+              <Route
+                path={`${paths.edit}/:problemId`}
+                component={ProblemEdit}
+                exact
+              />
+              <Route
+                path={`${paths.problem}/:problemId`}
+                component={ProblemShow}
+                exact
+              />
+              <Route path={paths.result} component={Result} />
+              <Route path={paths.mypage} component={Mypage} />
+            </Switch>
+          </AuthRedirect>
+        </Switch>
+      </Router>
     </div>
   );
 };
