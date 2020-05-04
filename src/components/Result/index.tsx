f680fc817dfba775bde4390c67c64058883bfcb6
Kentaro Suzuki <33363411+kentaro84207@users.noreply.github.com>
2020-05-04 11:46:17 +0900

    Implement result (#9)

--- src/components/Result/index.tsx
+++ src/components/Result/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import HomeContainer from 'containers/Home/HomeContainer';
-import Typography from '@material-ui/core/Typography';
 import ResultTable from 'components/common/table/resultTable';
+import useUsers from 'hooks/use-users';
+import Typography from '@material-ui/core/Typography';
 
 const Result: React.FC = () => {
+  const { users, loading } = useUsers();
+
   return (
     <HomeContainer>
       <Typography component="h1" variant="h5">
         リザルト
       </Typography>
-      <ResultTable />
+      <ResultTable users={users} loading={loading} />
     </HomeContainer>
   );
 };
