52672bba22886ad2e0d371e0c13e798c5f4cb5cf
kentaro84207 <kentaro84207@gmail.com>
2020-05-04 11:06:27 +0900

    Implemenet calc point

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,17 +1,36 @@
+/* eslint-disable no-console */
 import * as functions from 'firebase-functions';
 import admin from 'firebase-admin';
-// import { collectionName } from './services/constants';
+import { collectionName } from './services/constants';
 
 admin.initializeApp();
-// export const publishers = functions.region('asia-northeast1').https.onRequest(async (req, res) => {
-//   const snap = await admin
-//     .firestore().collection(collectionName.publishers).get();
-//   const data = snap.docs.map(doc => doc.data());
-//   res.send({ data });
-// });
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send('Hello from Firebase!');
-});
+const db = admin.firestore();
+db.settings({ timestampsInSnapshots: true });
+
+export const updatePoints = functions
+  .region('asia-northeast1')
+  .firestore.document('users/{uid}/problems/{pid}')
+  .onWrite(async (change, context) => {
+    const { uid, pid } = context.params;
+    const userRef = db.collection(collectionName.users).doc(uid);
+    const userSnapshot = await userRef.get();
+    const userData = userSnapshot.data();
+
+    const problemRef = db.collection(collectionName.problems).doc(pid);
+    const problemSnapshot = await problemRef.get();
+    const problemData = problemSnapshot.data();
+
+    if (!userData || !problemData) return;
+
+    const problemPoint = change.after.exists
+      ? Number(problemData.point)
+      : -1 * Number(problemData.point);
+    const currentPoints = Number(userData.points);
+    const newPoints = String(currentPoints + problemPoint);
+    const batch = db.batch();
+    batch.update(userRef, {
+      points: newPoints,
+    });
+    await batch.commit();
+    console.log(newPoints);
+  });
