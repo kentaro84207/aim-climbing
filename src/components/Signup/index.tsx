94bc008aa9dfaa0ae3d928fc762aa970b03ccb21
kentaro84207 <kentaro84207@gmail.com>
2020-05-10 15:38:14 +0900

    Remove unnecessary css

--- src/components/Signup/index.tsx
+++ src/components/Signup/index.tsx
@@ -10,12 +10,6 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
   form: {
     width: '100%',
     marginTop: theme.spacing(1),
