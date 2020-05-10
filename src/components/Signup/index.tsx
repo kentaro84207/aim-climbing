a45303502648f9fcd3287611ecd49b8d1c0677e7
Kentaro Suzuki <33363411+kentaro84207@users.noreply.github.com>
2020-05-10 10:43:27 +0900

    Implement validation (#14)

--- src/components/Signup/index.tsx
+++ src/components/Signup/index.tsx
@@ -30,13 +30,35 @@ const Signup: React.FC = () => {
   const { auth, db } = useContext(FirebaseContext);
   const { setCredential } = useContext(UserContext);
   const [values, setValues] = useState({ name: '', email: '', password: '' });
+  const [errors, setErrors] = useState({ email: false, password: false });
+  const [errorTexts, setErrorTexts] = useState({ email: '', password: '' });
   const history = useHistory();
+
+  const validationEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    const reg = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+    const invalid = !reg.test(value);
+    const text = invalid ? 'メールの形式が違います' : '';
+    setErrors({ ...errors, email: invalid });
+    setErrorTexts({ ...errorTexts, email: text });
+  };
+
+  const validationPassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    const invalid = value.length < 6;
+    const text = invalid ? '6文字以上で登録してください' : '';
+    setErrors({ ...errors, password: invalid });
+    setErrorTexts({ ...errorTexts, password: text });
+  };
 
   const updateValues = (e: React.ChangeEvent<HTMLInputElement>) => {
     setValues({
       ...values,
       [e.target.name]: e.target.value,
     });
+
+    if (e.target.name === 'password') validationPassword(e);
+    if (e.target.name === 'email') validationEmail(e);
   };
 
   const signupWithEmailAndPassword = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -85,6 +107,8 @@ const Signup: React.FC = () => {
           value={values.email}
           autoComplete="email"
           onChange={updateValues}
+          error={errors.email}
+          helperText={errorTexts.email}
         />
         <TextField
           variant="outlined"
@@ -92,12 +116,14 @@ const Signup: React.FC = () => {
           required
           fullWidth
           name="password"
-          label="パスワード(6桁以上)"
+          label="パスワード(6文字以上)"
           type="password"
           id="password"
           value={values.password}
           autoComplete="current-password"
           onChange={updateValues}
+          error={errors.password}
+          helperText={errorTexts.password}
         />
         <Button
           type="submit"
