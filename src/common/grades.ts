f5b021103ac842f70060b58b4788443815e4b990
Kentaro Suzuki <33363411+kentaro84207@users.noreply.github.com>
2020-05-22 08:52:38 +0900

    Add card color (#20)

--- src/common/grades.ts
+++ src/common/grades.ts
@@ -1,3 +1,12 @@
+import pink from '@material-ui/core/colors/pink';
+import orange from '@material-ui/core/colors/orange';
+import green from '@material-ui/core/colors/green';
+import lightBlue from '@material-ui/core/colors/lightBlue';
+import indigo from '@material-ui/core/colors/indigo';
+import red from '@material-ui/core/colors/red';
+import yellow from '@material-ui/core/colors/yellow';
+import grey from '@material-ui/core/colors/grey';
+
 type Grades = {
   [key: number]: string;
 };
@@ -16,6 +25,38 @@ const grades = {
   10: '三段',
   11: '四段',
   12: '五段',
+} as Grades;
+
+export const gradeColors = {
+  0: pink[300],
+  1: orange[500],
+  2: green[800],
+  3: lightBlue[100],
+  4: indigo[800],
+  5: red.A700,
+  6: yellow[500],
+  7: grey[500],
+  8: grey[800],
+  9: grey[800],
+  10: grey[800],
+  11: grey[800],
+  12: grey[800],
+} as Grades;
+
+export const contrastText = {
+  0: '#000',
+  1: '#000',
+  2: '#fff',
+  3: '#000',
+  4: '#fff',
+  5: '#fff',
+  6: '#000',
+  7: '#000',
+  8: '#fff',
+  9: '#fff',
+  10: '#fff',
+  11: '#fff',
+  12: '#fff',
 } as Grades;
 
 export default grades;
--- src/components/common/card/ProblemCard.tsx
+++ src/components/common/card/ProblemCard.tsx
@@ -2,12 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { User } from 'services/models/user';
 import { Problem } from 'services/models/problem';
-import grades from 'common/grades';
+import grades, { gradeColors, contrastText } from 'common/grades';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
-import CheckCircle from '@material-ui/icons/CheckCircle';
-import RadioButtonUncheckedIcon from '@material-ui/icons/RadioButtonUnchecked';
+import Check from '@material-ui/icons/Check';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
@@ -39,7 +38,7 @@ const useStyles = makeStyles(theme => ({
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
-    flex: '1 1 10%',
+    flex: '1 1 20%',
   },
 }));
 
@@ -49,30 +48,37 @@ const ProblemCard: React.FC<{ user: User | null; problem: Problem }> = ({ user, 
 
   const ascentUsers = problem && problem.users ? problem.users : [];
   const ascentStatus = user && user.id ? ascentUsers.includes(user.id) : false;
-  const statusLabel = ascentStatus ? <CheckCircle color="primary" /> : <RadioButtonUncheckedIcon />;
+  const statusLabel = ascentStatus ? <Check color="primary" /> : <div />;
 
   return (
     <Link className={classes.root} to={`problem/${id}`}>
       <Card className={classes.card}>
-        <CardContent className={classes.content}>
+        <CardContent
+          className={classes.content}
+          style={{ backgroundColor: `${gradeColors[grade]}` }}
+        >
           <div className={classes.column1}>
-            <Typography variant="subtitle1" color="textSecondary">
+            <Typography
+              variant="subtitle1"
+              color="textSecondary"
+              style={{ color: `${contrastText[grade]}` }}
+            >
               グレード
             </Typography>
-            <Typography component="h5" variant="h6">
+            <Typography component="h5" variant="h6" style={{ color: `${contrastText[grade]}` }}>
               {grades[grade]}
             </Typography>
           </div>
           <div className={classes.column2}>
-            <Typography variant="subtitle1" color="textSecondary">
+            <Typography variant="subtitle1" style={{ color: `${contrastText[grade]}` }}>
               セッター
             </Typography>
-            <Typography component="h5" variant="h6">
+            <Typography component="h5" variant="h6" style={{ color: `${contrastText[grade]}` }}>
               {setterName}
             </Typography>
           </div>
-          <div className={classes.center}>{statusLabel}</div>
         </CardContent>
+        <div className={classes.center}>{statusLabel}</div>
       </Card>
     </Link>
   );
