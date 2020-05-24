67f8da694b5a86127131f812080d7ac8e2a94efc
kentaro84207 <kentaro84207@gmail.com>
2020-05-25 08:42:03 +0900

    Add switch

--- src/components/common/card/CardIndex.tsx
+++ src/components/common/card/CardIndex.tsx
@@ -5,11 +5,19 @@ import Circular from 'components/common/atoms/Circular';
 import { UserContext } from 'contexts';
 import { Problem } from 'services/models/problem';
 import paths from 'paths';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
 
 type ProblemsProps = { problems: Problem[]; loading?: boolean };
 
 const CardIndex: React.FC<ProblemsProps> = ({ problems, loading }) => {
   const { user } = useContext(UserContext);
+  const [hideChecked, setHideChecked] = React.useState(false);
+
+  const handleChange = () => {
+    setHideChecked(!hideChecked);
+  };
 
   if (loading) return <Circular />;
 
@@ -28,8 +36,21 @@ const CardIndex: React.FC<ProblemsProps> = ({ problems, loading }) => {
 
   return (
     <>
+      <FormGroup>
+        <FormControlLabel
+          control={
+            <Switch
+              checked={hideChecked}
+              onChange={handleChange}
+              name="hideSwitch"
+              color="primary"
+            />
+          }
+          label="登った課題を非表示にする"
+        />
+      </FormGroup>
       {problems.map(problem => (
-        <ProblemCard user={user} problem={problem} key={problem.id} />
+        <ProblemCard user={user} problem={problem} key={problem.id} hidden={hideChecked} />
       ))}
     </>
   );
