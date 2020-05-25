b61bf0c833e9a7e8e5f6048c6643304fb5ced297
kentaro84207 <kentaro84207@gmail.com>
2020-05-26 08:56:38 +0900

    Implement sort

--- src/components/common/card/CardIndex.tsx
+++ src/components/common/card/CardIndex.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import ProblemCard from 'components/common/card/ProblemCard';
 import Circular from 'components/common/atoms/Circular';
@@ -8,15 +8,45 @@ import paths from 'paths';
 import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Switch from '@material-ui/core/Switch';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 
 type ProblemsProps = { problems: Problem[]; loading?: boolean };
 
 const CardIndex: React.FC<ProblemsProps> = ({ problems, loading }) => {
   const { user } = useContext(UserContext);
-  const [hideChecked, setHideChecked] = React.useState(false);
+  const [hideChecked, setHideChecked] = useState(false);
+  const [sort, setSort] = useState<string>('new');
 
   const handleChange = () => {
     setHideChecked(!hideChecked);
+  };
+
+  // FIXME more clean
+  const handlSort = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setSort(event.target.value as string);
+
+    const { value } = event.target;
+
+    if (value === 'new') {
+      problems.sort((a, b) => {
+        const secondsA = a.createdAt ? a.createdAt.seconds : 0;
+        const secondsB = b.createdAt ? b.createdAt.seconds : 0;
+
+        return secondsB - secondsA;
+      });
+    } else {
+      problems.sort((a, b) => {
+        const gradeA = a.grade;
+        const gradeB = b.grade;
+
+        if (value === 'hard') return gradeB - gradeA;
+
+        return gradeA - gradeB;
+      });
+    }
   };
 
   if (loading) return <Circular />;
@@ -36,6 +66,14 @@ const CardIndex: React.FC<ProblemsProps> = ({ problems, loading }) => {
 
   return (
     <>
+      <FormControl>
+        <InputLabel id="select-label">並び替え</InputLabel>
+        <Select labelId="select-label" id="select" value={sort} onChange={handlSort}>
+          <MenuItem value="new">新しい</MenuItem>
+          <MenuItem value="hard">難しい</MenuItem>
+          <MenuItem value="soft">易しい</MenuItem>
+        </Select>
+      </FormControl>
       <FormGroup>
         <FormControlLabel
           control={
