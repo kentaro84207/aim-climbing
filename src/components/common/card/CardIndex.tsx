0fd696980a1612814b535e96940d511e7fc0c350
kentaro84207 <kentaro84207@gmail.com>
2020-06-19 18:03:20 +0900

    Add mouse link

--- src/components/common/card/CardIndex.tsx
+++ src/components/common/card/CardIndex.tsx
@@ -84,6 +84,9 @@ const CardIndex: React.FC<ProblemsProps> = ({ problems }) => {
 
   return (
     <>
+      <Link style={{ marginBottom: '10px' }} to={`mypage/${user?.displayName}`}>
+        マウスになるにはこちらから
+      </Link>
       <div style={{ width: '100%' }}>
         <Box
           display="flex"
